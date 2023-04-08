b747e8f37e06a39acf019cfdd6927aefbc6d3871
reven-erlangga <reven.erlangga@gmail.com>
2023-04-08 10:39:45 +0700

    feature: adding feature publisher

---
+++ backend/src/publisher/publisher.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { PublisherService } from './publisher.service';
+
+@Controller('publisher')
+export class PublisherController {
+  constructor(private readonly publisherService: PublisherService) {}
+}
