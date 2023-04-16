5e630006d881b5decbef07af44c8971dc7070350
reven-erlangga <reven.erlangga@gmail.com>
2023-04-16 22:29:24 +0700

    fix: fixing some code

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,7 +1,20 @@
-import { Prisma } from '@prisma/client';
+import { Prisma, Role } from '@prisma/client';
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto implements Prisma.UserCreateInput {
+  id?: string;
+  email: string;
+  password?: string;
+  middleName?: string;
+  picture?: string;
+  role?: Role;
+  isValidate?: boolean;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+  oAuthTokens?: Prisma.OAuthTokenCreateNestedManyWithoutUserInput;
+  twoFactorAuths?: Prisma.TwoFactorAuthCreateNestedManyWithoutUserInput;
+  gender?: Prisma.GenderCreateNestedOneWithoutUserInput;
+  member?: Prisma.MemberCreateNestedOneWithoutUserInput;
   @IsString()
   @IsNotEmpty({
     message: 'Please insert your first name!',
